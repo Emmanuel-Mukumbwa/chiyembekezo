000029a535fe581c62d0b9132ef6a7686978223f
Emmanuel aRelic Mukumbwa <132208620+Emmanuel-Mukumbwa@users.noreply.github.com>
2026-06-30 14:38:26 +0200

    feat(frontend): add routes for all assessments, integrate ModalProvider and AuthProvider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,43 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import Assessments from './pages/Assessments';
+import PHQ9 from './pages/Assessments/PHQ9';
+import GAD7 from './pages/Assessments/GAD7';
+import Stress from './pages/Assessments/Stress';
+import Sleep from './pages/Assessments/Sleep';
+import Burnout from './pages/Assessments/Burnout';
+import Result from './pages/Assessments/Result';
+import Dashboard from './pages/Dashboard';
+import { AuthProvider } from './context/AuthContext';
+import { ModalProvider } from './context/ModalContext';
 import './styles/custom.css';
 
 function App() {
   return (
-    <Router>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* Other routes will be added later */}
-      </Routes>
-      <Footer />
-    </Router>
+    <AuthProvider>
+      <ModalProvider>
+        <Router>
+          <div className="app-wrapper d-flex flex-column min-vh-100">
+            <Navigation />
+            <div className="flex-grow-1">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/assessments" element={<Assessments />} />
+                <Route path="/assessments/phq9" element={<PHQ9 />} />
+                <Route path="/assessments/gad7" element={<GAD7 />} />
+                <Route path="/assessments/stress" element={<Stress />} />
+                <Route path="/assessments/sleep" element={<Sleep />} />
+                <Route path="/assessments/burnout" element={<Burnout />} />
+                <Route path="/assessments/result" element={<Result />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+              </Routes>
+            </div>
+            <Footer />
+          </div>
+        </Router>
+      </ModalProvider>
+    </AuthProvider>
   );
 }
 
