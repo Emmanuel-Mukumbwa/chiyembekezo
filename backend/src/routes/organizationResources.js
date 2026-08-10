0464dd781a1fadd9e3656fccafcef508e2a2e2e9
Emmanuel Mukumbwa <emukumbwa2419@gmail.com>
2026-08-10 12:39:35 +0200

    fix(org-routes): add upload middleware for organization resource creation/update

--- backend/src/routes/organizationResources.js
+++ backend/src/routes/organizationResources.js
@@ -2,14 +2,15 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
 const isOrgAdmin = require('../middleware/isOrgAdmin');
+const { uploadFile } = require('../middleware/upload');
 const resourceController = require('../controllers/organization/resourceController');
 
 router.use(auth, isOrgAdmin);
 
 router.get('/', resourceController.getResources);
 router.get('/:id', resourceController.getResourceById);
-router.post('/', resourceController.createResource);
-router.put('/:id', resourceController.updateResource);
+router.post('/', uploadFile, resourceController.createResource);
+router.put('/:id', uploadFile, resourceController.updateResource);
 router.delete('/:id', resourceController.deleteResource);
 
 module.exports = router;
